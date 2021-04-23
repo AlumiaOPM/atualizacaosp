db9bc6da693aec9e6a04bfbfb8479a39148bc9b1
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-04-23 10:16:29 -0300

    adding: 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Provider from '../../components/Provider';
+import { Button } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+
 import { useQuery } from '../../hooks/';
+
+import './index.css';
 
 const NotFound = (props) => {
 	const query = useQuery();
 
 	return (
-		<div>
-			<div>
-				<h3>404 página não encontrada</h3>
-				<h2>Nenhuma página encontrada para <code>{`/${query.get("query")}`}</code></h2>
-			</div>
+		<div className="notFound">
+			<Provider>
+
+				<div className="notFound-container">
+					<h3>404 página não encontrada</h3>
+					<h2>Nenhuma página encontrada para <code>{`/${query.get("query")}`}</code></h2>
+
+					<Link to="/">
+						<Button
+							colorScheme="green"
+							margin="40px auto"
+						>
+							<ArrowBackIcon
+								m="2"
+							/>
+							Conheça nossos cursos
+						</Button>
+					</Link>
+				</div>
+
+			</Provider>
 		</div>
 	);
 }
